57a363b57028af7914d02670ef9100356ef01c18
JohnnyBakaas <johnny@bakaas-cadconsult.no>
2022-10-20 23:37:45 +0200

    Sjakk nå med brikker

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-"strickt";
+"use strict";
 
 let chessBoardExsists = false;
 
@@ -8,15 +8,16 @@ let chessBoard = "";
 
 let generertID = 0;
 
-let bricks = ["Rook", "Knight", "Bishop", "King", "Quin", "Pawn"];
+let bricks = ["♟", "♜", "♞", "♝", "♛", "♚", "♝", "♞", "♜"];
 
-const test = () => {
+const start = () => {
   makeChessBoard();
   document.getElementById("yes").innerHTML = `${chessBoard}`;
+  spawnPieces();
 };
 
 const makeChessBoard = () => {
-  if (!chessBoardExsists) {
+  if (chessBoard === "") {
     for (let i = 0; i < 8; i++) {
       chessBoard = chessBoard += '<div class="horisontalt">';
       for (let y = 0; y < 8; y++) {
@@ -34,9 +35,7 @@ const makeChessBoard = () => {
       changeColor();
     }
     chessBoardExsists = true;
-    spawnBricks();
   } else {
-    console.log("STOP");
   }
 };
 
@@ -48,10 +47,22 @@ const changeColor = () => {
   }
 };
 
-const spawnBricks = () => {
-  for (let i = 0; i < 5; i++) {
-    let y = '"' + i + '"';
-    console.log(y);
-    //document.getElementById(y).innerHTML = "<div>reeeeeeee</div>";
+const spawnPiece = (id, content, color) => {
+  document.getElementById(id).innerHTML =
+    '<div class="' + color + ' piece">' + content + "</div>";
+};
+
+const spawnPieces = () => {
+  let curentPosition = 0;
+  for (let i = 1; i < bricks.length; i++) {
+    spawnPiece(curentPosition, bricks[i], "red");
+    spawnPiece(curentPosition + 8, bricks[0], "red");
+    curentPosition++;
+  }
+  curentPosition = 8 * 8 - 1;
+  for (let i = 1; bricks.length > i; i++) {
+    spawnPiece(curentPosition, bricks[i], "notWhite");
+    spawnPiece(curentPosition - 8, bricks[0], "notWhite");
+    curentPosition--;
   }
 };
