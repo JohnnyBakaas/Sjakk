ca9b4b8efdcf327cbfe8a20953a9de76ce9ea3be
JohnnyBakaas <johnny@bakaas-cadconsult.no>
2022-10-24 13:03:20 +0200

    Mer frammgang

--- mainTwo.js
+++ mainTwo.js
@@ -2,11 +2,36 @@
 
 //når ferdig: lag en AI knap som gjør de samme movsa som magnus karlsen gjorde mot amrekaneren og FF
 
+const TYPE = 0;
+const TEAM_COLOR = 1;
+const AVATAR = 2;
+const BRETTFARGE = 3;
+
+let boardArrId = new Array(8);
+let virginBoardArr = new Array(8);
 let boardArr = new Array(8);
 
 let lightOrDark = "light";
 
 let bricks = ["♟", "♜", "♞", "♝", "♛", "♚", "♝", "♞", "♜"];
+
+//makes ID array
+const makeIdArray = () => {
+  for (let i = 0; i < boardArrId.length; i++) {
+    boardArrId[i] = new Array(8);
+  }
+  let id = 0;
+  for (let i = 0; i < boardArrId.length; i++) {
+    for (let j = 0; j < boardArrId[i].length; j++) {
+      boardArrId[i][j] = id;
+      id++;
+    }
+  }
+  console.table(boardArrId);
+};
+
+//takes contetnt of the selected pice and stores it
+let selectedContent = [];
 
 //orderd as black
 let bricksName = [
@@ -21,17 +46,17 @@ let bricksName = [
   "Rook",
 ];
 
-const makeChessBoardArr = () => {
+const makeChessBoardArr = (arr) => {
   // makes a 2d array to represent a chessbord
-  for (let i = 0; i < boardArr.length; i++) {
-    boardArr[i] = new Array(8);
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = new Array(8);
   }
 
-  for (let i = 0; i < boardArr.length; i++) {
-    for (let j = 0; j < boardArr.length; j++) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
       //makes a array with te information of the place, could have used object, but im to lazy to learn about that just yett
-      //                type, team, avatar, brettfarge
-      boardArr[i][j] = [null, null, null, lightOrDark];
+      //                type, team, avatar, brettfargen
+      arr[i][j] = [null, null, null, lightOrDark];
       changeLightOrDark();
     }
     changeLightOrDark();
@@ -50,36 +75,25 @@ let changeLightOrDark = () => {
 
 const spawnPiecesInArr = () => {
   for (let i = 0; i < boardArr.length; i++) {
-    spawnPawn(boardArr, 1, i, "black");
+    spawnPawn(boardArr, TEAM_COLOR, i, "black");
     spawnPawn(boardArr, 6, i, "white");
-    console.log("REEEEE");
   }
-  spawnBacklineBlack(boardArr, 0);
-  spawnBacklineWhite(boardArr, 7);
+  spawnBackline(boardArr, 0, "black");
+  spawnBackline(boardArr, 7, "white");
 };
 
 const spawnPawn = (arr, height, width, team) => {
-  arr[height][width][0] = bricksName[0];
-  arr[height][width][1] = team;
-  arr[height][width][2] = bricks[0];
+  arr[height][width][TYPE] = bricksName[0];
+  arr[height][width][TEAM_COLOR] = team;
+  arr[height][width][AVATAR] = bricks[0];
 };
 
-const spawnBacklineBlack = (arr, height) => {
+const spawnBackline = (arr, height, team) => {
   for (let i = 0; i < boardArr.length; i++) {
     let j = i + 1;
-    arr[height][i][0] = bricksName[j];
-    arr[height][i][1] = "black";
-    arr[height][i][2] = bricks[j];
-  }
-};
-
-const spawnBacklineWhite = (arr, height) => {
-  let j = 1;
-  for (let i = 7; i > -1; i--) {
-    arr[height][i][0] = bricksName[j];
-    arr[height][i][1] = "white";
-    arr[height][i][2] = bricks[j];
-    j++;
+    arr[height][i][TYPE] = bricksName[j];
+    arr[height][i][TEAM_COLOR] = team;
+    arr[height][i][AVATAR] = bricks[j];
   }
 };
 
@@ -92,28 +106,89 @@ const makeTheHTML = () => {
   for (let i = 0; i < boardArr.length; i++) {
     content = content + '<div class="horisontalt">';
     for (let j = 0; j < 8; j++) {
-      content = content + '<div class="' + boardArr[i][j][3] + '">';
-      if (boardArr[i][j][0] !== null) {
+      content = content + '<div class="' + boardArr[i][j][BRETTFARGE] + '">';
+      if (boardArr[i][j][TYPE] !== null) {
         content =
           content +
           '<span class="' +
-          boardArr[i][j][1] +
-          '">' +
-          boardArr[i][j][2] +
+          boardArr[i][j][TEAM_COLOR] +
+          '" ' +
+          addId(i, j) +
+          ">" +
+          boardArr[i][j][AVATAR] +
           "</span>";
       }
       content = content + "</div>";
     }
     content = content + "</div>";
   }
-  console.log(content);
   return content;
 };
 
-makeChessBoardArr();
-spawnPiecesInArr();
-boardArr[0][0][0] = "Test";
-console.log(boardArr);
-makeTheHTML();
+const addId = (i, j) => {
+  return "id(" + boardArrId[i][j] + ")";
+};
 
-document.getElementById("chess-bord").innerHTML = makeTheHTML();
+const handlePiece = (i, j) => {
+  console.log(i, j);
+  handlePiecePawnBlack(i, j);
+  renderHTML();
+};
+
+const handlePiecePawnBlack = (i, j) => {
+  if (boardArr[i][j][TEAM_COLOR] == "black") {
+    if (
+      boardArr[i + 1][j][BRETTFARGE] == "potential-moves-dark" ||
+      boardArr[i + 1][j][BRETTFARGE] == "potential-moves-light"
+    ) {
+      if (i == 1) {
+        boardArr[i + 1][j][BRETTFARGE] = virginBoardArr[i + 1][j][BRETTFARGE];
+        boardArr[i + 2][j][BRETTFARGE] = virginBoardArr[i + 2][j][BRETTFARGE];
+      } else {
+        boardArr[i + 1][j][BRETTFARGE] = virginBoardArr[i + 1][j][BRETTFARGE];
+      }
+      boardArr[i][j][BRETTFARGE] = virginBoardArr[i][j][BRETTFARGE];
+    } else {
+      boardArr[i][j][BRETTFARGE] = "marked-piece";
+      if (i == 1) {
+        if (boardArr[i + 1][j][BRETTFARGE] == "dark") {
+          boardArr[i + 1][j][BRETTFARGE] = "potential-moves-dark";
+        } else {
+          boardArr[i + 1][j][BRETTFARGE] = "potential-moves-light";
+        }
+        if (boardArr[i + 2][j][BRETTFARGE] == "dark") {
+          boardArr[i + 2][j][BRETTFARGE] = "potential-moves-dark";
+        } else {
+          boardArr[i + 2][j][BRETTFARGE] = "potential-moves-light";
+        }
+      } else {
+        if (boardArr[i + 1][j][BRETTFARGE] == "dark") {
+          boardArr[i + 1][j][BRETTFARGE] = "potential-moves-dark";
+        } else {
+          boardArr[i + 1][j][BRETTFARGE] = "potential-moves-light";
+        }
+      }
+      //endre funksjonen i til de som blir higlighta
+    }
+    //moveThePawn
+  }
+};
+
+const moveThePiece = (positionX, positionY) => {
+  if (boardArr[positionX][positionY][BRETTFARGE].includes("potential-moves")) {
+    console.log("Det funker!");
+  }
+};
+
+makeIdArray();
+makeChessBoardArr(boardArr);
+makeChessBoardArr(virginBoardArr);
+spawnPiecesInArr();
+
+const renderHTML = () => {
+  makeTheHTML();
+  document.getElementById("chess-bord").innerHTML = makeTheHTML();
+};
+
+renderHTML();
+console.log(virginBoardArr);
